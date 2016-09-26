2ab4f6b5a8f5292cb91bf580cb4a6408ddedf261
Daniel Rauf <danielr@kainos.com>
2016-09-26 14:41:51 +0200

    Implement deleting notes

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Note from './Note';
 
 
-export default ({notes}) => (
-  <ul>{notes.map(note =>
-    <li key={note.id}>{note.task}</li>
+// dummy implementation to prevent from crashing if onDelete is not provided
+export default ({notes, onDelete=() => {}}) => (
+  <ul>{notes.map(({id, task}) =>
+    <li key={id}>
+      <Note
+        onDelete={onDelete.bind(null, id)}
+        task={task} />
+    </li>
   )}</ul>
 )

c01a70eaea3a9b5f0bb2753157296ba6228327b8
Daniel Rauf <danielr@kainos.com>
2016-09-27 15:19:46 +0200

    Add styling

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import classnames from 'classnames';
 
 
-const Editable = ({editing, value, onEdit, ...props}) => {
+const Editable = ({editing, value, onEdit, className, ...props}) => {
   if (editing) {
-    return <Editable.Edit value={value} onEdit={onEdit} {...props} />;
+    return <Editable.Edit
+      className={className}
+      value={value}
+      onEdit={onEdit}
+      {...props} />;
   }
 
-  return <Editable.Value value={value} />;
+  return <Editable.Value className={className} value={value} />;
 };
 
-Editable.Value = ({value, ...props}) => <span {...props}>{value}</span>;
+Editable.Value = ({value, className, ...props}) => <span className={classnames('value', className)} {...props}>
+    {value}
+  </span>;
 
 class Edit extends React.Component {
   render() {
-    const {value, onEdit, ...props} = this.props;
+    const {className, value, onEdit, ...props} = this.props;
 
     return (<input
       type="text"
+      className={classnames('edit', className)}
       autoFocus={true}
       defaultValue={value}
       onBlur={this.finishEdit}
